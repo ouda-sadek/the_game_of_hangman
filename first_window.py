ad25e07d4e484ec78d4f2ddb4810de774f777043
ouda-sadek <ouda.sadek@laplateforme.io>
2025-01-21 19:09:08 +0100

    draw the lines of the hanged man

--- first_window.py
+++ first_window.py
@@ -1,15 +1,49 @@
 import pygame
 
+
+# Initialize Pygame 
 pygame.init()
 
-# Define the window size
-window_width = 400
-window_height = 400
-
-# Create a Pygame window and set the title, ((window_width, window_height) is a tuple 
-window = pygame.display.set_mode((window_width, window_height))
+# Create a Pygame window and set the size ((window_width, window_height) is a tuple 
+screen = pygame.display.set_mode((840, 600))
 pygame.display.set_caption("Pygame Window")
 
+
+# Define the line color and starting and ending positions
+#Define the line position with (X, Y)
+start_pos = (100, 400)
+end_pos = (400, 400)
+
+# Define the line color (RGB)
+line_color = line_color = (255, 255, 255)
+
+# Draw a line on the screen
+pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
+
+# Define a second line
+start_pos = (150, 100)
+end_pos = (150, 400)
+pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
+
+# Define a third line
+start_pos = (150, 100)
+end_pos = (350, 100)
+pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
+
+# Define a fourth line
+start_pos = (350, 100)
+end_pos = (350, 150)
+pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
+
+# Define a fifth line
+start_pos = (200, 100)
+end_pos = (150, 150)
+pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
+
+# Display the window
+pygame.display.flip()
+
+# Main loop to keep the window open until user closes it
 running = True
 while running:
     # Process events
@@ -19,4 +53,4 @@ while running:
 
 pygame.quit()
 
-    
+   
