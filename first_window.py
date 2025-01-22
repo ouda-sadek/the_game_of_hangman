cbf121ca1db3ec4c8780eef889d364dd5af321f0
ouda-sadek <ouda.sadek@laplateforme.io>
2025-01-22 03:15:36 +0100

    optimization code

--- first_window.py
+++ first_window.py
@@ -1,47 +1,33 @@
 import pygame
 
-
 # Initialize Pygame 
 pygame.init()
 
 # Create a Pygame window and set the size ((window_width, window_height) is a tuple 
 screen = pygame.display.set_mode((840, 600))
-pygame.display.set_caption("Pygame Window")
+pygame.display.set_caption("THE GAME OF HANGMAN")
 
+#Define color & size
+WHITE = (250, 250, 250)
 
-# Define the line color and starting and ending positions
-#Define the line position with (X, Y)
-start_pos = (100, 400)
-end_pos = (400, 400)
-
-# Define the line color (RGB)
-line_color = line_color = (255, 255, 255)
-
-# Draw a line on the screen
-pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
-
-# Define a second line
-start_pos = (150, 100)
-end_pos = (150, 400)
-pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
-
-# Define a third line
-start_pos = (150, 100)
-end_pos = (350, 100)
-pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
-
-# Define a fourth line
-start_pos = (350, 100)
-end_pos = (350, 150)
-pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
-
-# Define a fifth line
-start_pos = (200, 100)
-end_pos = (150, 150)
-pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
-
-# Display the window
-pygame.display.flip()
+# Define the line position with (X, Y)
+def draw_line():
+    lines = [
+        # Define the first horizontal line
+        ((100, 400), (400, 400)),  
+        # Define the second vertical line
+        ((150, 100), (150, 400)), 
+        # Define the third horizontal line
+        ((150, 100), (350, 100)),  
+        # Define the fourth vertical line
+        ((350, 100), (350, 150)),  
+        # Define the fifth diagonal line
+        ((200, 100), (150, 150))   
+    ] 
+    # Define the line color (RGB)
+    line_color = line_color = (WHITE)
+    for start_pos, end_pos in lines:
+        pygame.draw.line(screen, line_color, start_pos, end_pos, 5)
 
 # Main loop to keep the window open until user closes it
 running = True
@@ -50,7 +36,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    draw_line()
+    # Display the window
+    pygame.display.flip()
 pygame.quit()
 
    
